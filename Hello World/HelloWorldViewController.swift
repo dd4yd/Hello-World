de4e890cda1f46468eb1af5fd0d65b64adcb993e
David Dean <daviddean@mwc-032200.dhcp.missouri.edu>
2018-01-26 20:12:11 -0600

    First implementation of requirements

---
+++ Hello World/HelloWorldViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HelloWorldViewController.swift
+//  Hello World
+//
+//  Created by David Dean on 1/23/18.
+//  Copyright © 2018 David Dean. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        messageLabel.text = "Welcome!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func helloTapped(_ sender: Any) {
+        messageLabel.text = "Hello World!"
+    }
+    
+    @IBAction func clearTapped(_ sender: Any) {
+        messageLabel.text = ""
+    }
+    
+
+}
